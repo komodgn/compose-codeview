fc061d1b972278e96d77cf947a4ca0b5da990265
komodgn <a01075251881@gmail.com>
2026-05-15 16:44:00 +0900

    refactor(compose): Support Annotation token and refactor color constants

---
+++ codeview/compose/src/commonMain/kotlin/io/github/komodgn/codeview/compose/theme/Color.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2026 komodgn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.komodgn.codeview.compose.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Red = Color(0xFFFF7B72)
+val Yellow = Color(0xFFF2CC60)
+val Orange = Color(0xFFD2A8FF)
+val SkyBlue = Color(0xFFA5D6FF)
+val Gray = Color(0xFF8B949E)
+val White = Color(0xFFC9D1D9)
+val LightOrange = Color(0xFFFFA657)
